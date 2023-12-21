e0aae4ff2b4e3dd6588e08527e8946a3137c8cbf
Yaroslava <yaroslava.hovenko@gmail.com>
2023-12-21 20:47:25 +0200

    add 6 task

--- js/task-6.js
+++ js/task-6.js
@@ -1,8 +1,34 @@
+const input = document.querySelector('input');
+const boxes = document.querySelector('#boxes');
+
+const controls = document.querySelector('#controls');
+const quantityDiv = controls.querySelector('input');
+
+const btnCreate = controls.querySelector('[data-create]');
+const btnDestroy = controls.querySelector('[data-destroy]');
+const commonDiv = document.querySelector('#boxes');
+
+btnCreate.addEventListener('click', () => {
+  let n = quantityDiv.value;
+  console.log(n);
+
+  let side = 30;
+  for (let i = 1; i <= n; i += 1) {
+    const square = document.createElement('div');
+    square.style.width += `${side}px`;
+    square.style.height += `${side}px`;
+    side += 10;
+    commonDiv.appendChild(square);
+    square.style.backgroundColor = getRandomHexColor();
+  }
+});
+
+btnDestroy.addEventListener('click', () => {
+  commonDiv.innerHTML = '';
+});
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
-
-const input = document.querySelector('input');
-const boxes = document.querySelector('#boxes');
